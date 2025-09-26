b216853a8beea8e3bc6a44aa64e6d3e3c922fd05
Mogenio <gabriel.mogenio00@gmail.com>
2025-09-26 21:11:14 +0000

    operação aritméticos

---
+++ exercicio5.py
@@ -0,0 +1,69 @@
+#05 Faça um programa que leia um número inteiro e mostre 
+#na tela o seu sucessor e o seu antecessor.
+
+#numero = int(input('Digite um número: '))
+
+#print('O número é: {}' .format(numero))
+
+#sucessor = numero + 1
+#antecessor = numero - 1
+
+#print('O sucessor do número é: {} '.format(sucessor))
+#print('O antecessor do número é: {} '.format(antecessor))
+
+#06 crie um algoritmo que leia um número e mostre o seu dobro
+#triplo e raiz quadrada
+
+#numero = int(input('Digite um número: '))
+#dobro = numero * 2
+#triplo = numero * 3
+#raiz = dobro ** 0.5
+#raiz2 = triplo ** 0.5
+
+#print('O número digitado foi {}, e o dobro dele é {} e a raiz quadrada dele é {} '.format(numero, dobro, raiz))
+#print('O número digitado foi {}, e o triplo dele é {} e a sua raiz quadrada é {} '.format(numero, triplo, raiz2))
+
+#07 Desenvolva um programa que leia as duas notas de um aluno,
+# calcule e mostre sua média.
+
+#nome = input('Digite seu nome: ')
+#n1 = int(input('Digite a primeira nota: '))
+#n2 = int(input('Digite a segunda nota: '))
+#media = (n1 + n2)/2
+
+#print('A média do aluno {} foi {} '.format(nome, media))
+
+
+#08 Escreva um programa que leia um valor em metros e o exiba
+#convertido em centimentros e milimetros.
+
+#metros = float(input('Digite um valor em metros: '))
+#centimetros = metros * 100
+#milimetros = metros * 1000
+
+#print('O valor do metros é {}, e o centimetros dele é {} e o seu milimetros é {} '.format(metros, centimetros, milimetros))
+
+#09 Faça um programa que leia um número qualquer e mostre
+#na tela a sua tabuada 
+
+#numero = int(input('Digite um número a ser multiplicado: '))
+#n1 = numero * 1
+#n2 = numero * 2
+#n3 = numero * 3
+#n4 = numero * 4
+#n5 = numero * 5
+#n6 = numero * 6
+#n7 = numero * 7
+#n8 = numero * 8
+#n9 = numero * 9
+#n10 = numero * 10
+
+#print(n1, n2, n3, n4, n5, n6, n7, n8, n9, n10)
+
+#crie um programa que leia quanto dinheiro uma pessoa tem na carteira
+#e mostre quantos Dólares ela pode comprar.
+
+#carteira = int(input('Digite o valor que você tem:$'))
+#dolar = carteira / 5.34
+
+#print('Tenha na carteira ${}, e vou poder comprar em US${} em dolar' .format(carteira, dolar))
