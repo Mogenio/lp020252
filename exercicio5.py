b8a87b2562cead91a110e4150b72ce3699179e95
Mogenio <gabriel.mogenio00@gmail.com>
2025-11-06 20:18:19 +0000

    NOVO

--- exercicio5.py
+++ exercicio5.py
@@ -103,5 +103,15 @@ def q13():
     f = 9 * c / 5 + 32
     print('A temperatura de {}°C corresponde a {}°F!'.format(c, f))
 
+# 13- Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado
+# e a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar sabendo que o carro custa 
+# R$60 por dia e R$0,15 por Km rodado
+def q13():
+    dias = int(input('Quantos dias alugados? '))
+    km = float(input('Quantos Km rodados? '))
+    pago = (dias * 60) + (km * 0.15)
+    print('O total a pagar é de R${:.2f}.'.format(pago))
+
+
 questao = int(input('Questão a ser executada: '))
 eval(f'q{questao}()')
