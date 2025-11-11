9af3bc2f942af4f30ac5e5079ec677717239de06
Mogenio <gabriel.mogenio00@gmail.com>
2025-11-11 21:30:00 +0000

    a

---
+++ modulos.py
@@ -0,0 +1,29 @@
+import random
+# import math
+# num = int(input('Digite um número: '))
+# raiz = math.sqrt(num)
+# print('A raiz de {} é igual a {}'.format(num, raiz))
+
+# 01- Crie um programa que leia um número real qualquer pelo teclado e mostre
+# na tela a sua porção inteira 
+
+
+# 02- Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triangulo 
+# retangulo, calcule e mostre o comprimento da hipotenusa
+
+# 03- Faça um programa que leia um angulo qualquer e mostre na tela o valor da seno, cosseno
+# e tangente desse angulo 
+
+# 04- Um professor quer sortea um dos seus quatros alunos para apagar o quadro. Faça um programa
+# que ajude ele, lendo o nome deles e escrevendo o nome escolhido 
+
+
+# 05- O mesmo do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos
+# Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada
+
+
+# 06- Faça um programa em Python que abra e reproduza o audio de um arquivo MP3
+
+
+questao = int(input('Questao a ser executada: '))
+eval(f'q{questao}()')
