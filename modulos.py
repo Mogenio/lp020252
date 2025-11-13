e49e329812ef452e5ba5f7c69d3825792937a3c7
Mogenio <gabriel.mogenio00@gmail.com>
2025-11-13 20:46:06 +0000

    --

--- modulos.py
+++ modulos.py
@@ -36,10 +36,30 @@ def q2():
 
 # 04- Um professor quer sortea um dos seus quatros alunos para apagar o quadro. Faça um programa
 # que ajude ele, lendo o nome deles e escrevendo o nome escolhido 
+def q4():
+    from random import choice
+    n1 = str(input('Primeiro aluno: '))
+    n2 = str(input('Segundo aluno: '))
+    n3 = str(input('Terceiro aluno: '))
+    n4 = str(input('Quarto aluno: '))
+    lista = [n1, n2, n3, n4]
+    escolhido = choice(lista)
+    print('O aluno escolhido foi {}'.format(escolhido))
 
 
 # 05- O mesmo do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos
 # Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada
+def q5():
+    from random import shuffle
+    n1 = str(input('Primeiro aluno: '))
+    n2 = str(input('Segundo aluno: '))
+    n3 = str(input('Terceiro aluno: '))
+    n4 = str(input('Quarto aluno: '))
+    lista = [n1, n2, n3, n4]
+    shuffle(lista)
+    print('A ordem de apresentação será ')
+    print(lista)
+
 
 
 # 06- Faça um programa em Python que abra e reproduza o audio de um arquivo MP3
